2f74a978d3a7c067efb93d4468c6871d14eb3cb0
SergoShe <snow5373@gmail.com>
2024-06-05 21:56:54 +0700

    Init repository

---
+++ src/main/java/com/github/SergoShe/Main.java
@@ -0,0 +1,9 @@
+package com.github.SergoShe;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
